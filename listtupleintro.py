914fb44069d0fbdbcf293e5949086820d6e81f13
Mayank-R <ray723260@gmail.com>
2025-07-17 22:52:50 +0530

    Add files via upload

---
+++ listtupleintro.py
@@ -0,0 +1,67 @@
+# List = ["Aman",123,132.3,True]
+# print(List)
+# List.append("Mayank")
+# print(List)
+# List.pop(3)
+# print(List)
+# arrange = [11,45,7,89,65,41,35,1,1,0,1]
+# arrange.sort()
+# print(arrange)
+# arrange.insert(0,"Aman")
+# print(arrange)
+# arrange.remove("Aman")
+# print(arrange)
+# print(type(arrange))
+# print(type(List))
+
+# tuple1 = (104,45,41)
+# tuple2 = (10,45,6,4)
+# concinate = tuple1+tuple2
+# print(concinate)
+# print(type(tuple1))
+# print(tuple1.index(45))ṇ
+# print(104 in concinate)
+# print(000 in concinate)
+# print(len(concinate))
+# print(min(concinate))
+# print(max(concinate))
+
+# slice = concinate[0:2]
+# print(slice)
+
+# Practice Set
+
+# fruits = []
+# f1=input("Enter Fruit 1 : ")
+# f2=input("Enter Fruit 2 : ")
+# f3=input("Enter Fruit 3 : ")
+# f4=input("Enter Fruit 4 : ")
+# f5=input("Enter Fruit 5 : ")
+# f6=input("Enter Fruit 6 : ")
+# f7=input("Enter Fruit 7 : ")
+# fruits = [f1,f2,f3,f4,f5,f6,f7]
+# print(fruits)
+
+# practice 2
+# marks = []
+# rohan = input("Enter marks : ")
+# mayank = input("Enter marks : ")
+# aman = input("Enter marks : ")
+# gunja = input("Enter marks : ")
+# chanda = input("Enter marks : ")
+# arun = input("Enter marks : ")
+# marks = [rohan,mayank,aman,gunja,chanda,arun]
+# marks.sort()
+# print(marks)
+
+# practice 3
+
+# numbers = [45,44,16,54]
+#  sum = numbers[0]+numbers[1]+numbers[2]+numbers[3]
+# print(sum(numbers))
+
+# a = [10,0,6,0,14,0,65]
+# countss = a.count(0)
+# print(countss)
+# pos = a.index(0)
+# print(pos)
