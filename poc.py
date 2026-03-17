a2a371585826c7a63005fdc354ec5952fec6a315
Mayank Rai <ray723260@gmail.com>
2026-03-17 18:00:39 +0530

    lists

---
+++ poc.py
@@ -0,0 +1,12 @@
+a = "gOOd morNing"
+a = a.title()
+print(a)
+
+
+Product = ['Pencil', 'Pen', 'Eraser', 'Pencil Box', 'Scale']
+Price= [5, 10, 2, 20, 12]  
+Brand = ['Camlin', 'Rotomac', 'Nataraj', 'Camel', 'Apsara']
+Stationery = [Product, Price, Brand]
+print(Stationery[0])
+print(Stationery[0][0])
+print(Stationery[0][1])
